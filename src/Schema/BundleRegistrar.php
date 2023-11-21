03f556ceedd9b7a32ce1c6ee82444462f33385c6
Karim Mohamed <komicho1996@gmail.com>
2023-11-22 00:21:31 +0200

    WIP: create a schema for the bundle registrar

---
+++ src/Schema/BundleRegistrar.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Obelaw\Schema;
+
+use Obelaw\Framework\Pipeline\Bundles\Configurations;
+
+class BundleRegistrar
+{
+    const MODULE = 'module';
+    const PLUGIN = 'plugin';
+
+    private static $paths = [
+        self::MODULE => [],
+        self::PLUGIN => [],
+    ];
+
+    private static $providers = [];
+
+    public static function register($type, $bundleName, $path, $configCallback = null)
+    {
+        self::$paths[$type][$bundleName] = str_replace('\\', '/', $path);
+
+        if ($configCallback) {
+            $configurations = new Configurations;
+            $configCallback($configurations);
+
+            if ($providers = $configurations->getProviders()) {
+                self::$providers[$bundleName] = $providers;
+            }
+        }
+    }
+
+    public static function getPaths($type)
+    {
+        return self::$paths[$type];
+    }
+
+    public static function getProviders()
+    {
+        return self::$providers;
+    }
+}
