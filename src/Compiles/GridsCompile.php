fecbb4da3a54c36747f9ce23c20a2b7dec085ecd
Karim Mohamed <komicho1996@gmail.com>
2024-01-27 18:51:38 +0200

    Feat: compile action components for grids

--- src/Compiles/GridsCompile.php
+++ src/Compiles/GridsCompile.php
@@ -4,6 +4,7 @@ namespace Obelaw\Compiles;
 
 use Illuminate\Console\OutputStyle;
 use Obelaw\Compiles\Abstracts\Compile;
+use Obelaw\Schema\Grid\Action;
 use Obelaw\Schema\Grid\Button;
 use Obelaw\Schema\Grid\CTA;
 use Obelaw\Schema\Grid\Table;
@@ -36,9 +37,14 @@ class GridsCompile extends Compile
                     $table = new Table;
                     $CTA = new CTA;
                     $button = new Button;
+                    $actions = new Action;
 
                     if (method_exists($gridClass, 'createButton')) {
                         $gridClass->createButton($button);
+                    }
+
+                    if (method_exists($gridClass, 'actions')) {
+                        $gridClass->actions($actions);
                     }
 
                     $gridClass->table($table);
@@ -49,6 +55,7 @@ class GridsCompile extends Compile
                         'where' => (property_exists($gridClass, 'where')) ? $gridClass->where : null,
                         'filter' => (property_exists($gridClass, 'filter')) ? $gridClass->filter : null,
                         'buttons' => $button->getButtons(),
+                        'actions' => $actions->getActions(),
                         'rows' => $table->getColumns(),
                         'CTAs' => $CTA->getCalls(),
                     ];
