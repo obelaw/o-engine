e6c08cdd93b6388f14c1d3a84022e097c4e21218
Karim Mohamed <komicho1996@gmail.com>
2024-02-21 01:52:49 +0200

    Fix: compile the dashboard routes in a plugin

--- src/Compiles/Plugins/RoutesDashboardPluginCompile.php
+++ src/Compiles/Plugins/RoutesDashboardPluginCompile.php
@@ -8,37 +8,24 @@ use Obelaw\Facades\Bundles;
 
 class RoutesDashboardPluginCompile extends RoutesDashboardCompile
 {
-    private $routes = [];
-
-    private function setRoute($id, $path)
-    {
-        $route[$id] = $path;
-
-        $this->routes = array_merge(Bundles::getDashboardRoutes(), $route);
-    }
-
-    private function getRoutes()
-    {
-        return $this->routes;
-    }
-
     public function scanner($paths, OutputStyle $consoleOutput = null)
     {
         $consoleOutput?->writeln('Routes for plugin Compile...');
 
         $this->routes = Bundles::getDashboardRoutes();
 
+        $outRoutes = [];
         foreach ($paths as $id => $path) {
             $pathRoutesFile = $path . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'dashboard.php';
 
             if (file_exists($pathRoutesFile)) {
-                $this->setRoute($id, $pathRoutesFile);
+                $outRoutes[$id] = $pathRoutesFile;
             }
         }
 
         $consoleOutput?->writeln('Routes for plugin Compiled.');
         $consoleOutput?->newLine();
 
-        return $this->getRoutes();
+        return array_merge(Bundles::getDashboardRoutes(), $outRoutes);
     }
 }
